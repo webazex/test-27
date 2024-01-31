74cdd2da5d452abe883a51847de58dd79d972923
Anton <webazex@gmail.com>
2024-01-31 14:28:07 +0200

    add archive for cpt

---
+++ wp-content/themes/dtw/archive.php
@@ -0,0 +1,23 @@
+<?php
+get_header();
+
+use Webazex\App\Core\Page\Page as Page;
+
+$thisPostType = get_queried_object()->name;
+?>
+<main>
+    <?php
+    if ($thisPostType == 'news'):
+        $newsCats = getNewsCats();
+        Page::section('news', [
+            'title' => __('Новини', 'dwt'),
+            'cats' => $newsCats,
+            'news' => getAllNews()
+        ]);
+    else:
+        Page::section('demo');
+    endif;
+    ?>
+
+</main>
+<?php get_footer(); ?>
