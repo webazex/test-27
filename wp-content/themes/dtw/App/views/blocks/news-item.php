c38d65ea1457551a8a09b13cc611fd7095d157be
Anton <webazex@gmail.com>
2024-02-04 11:58:55 +0200

    fix news-item block, added catrgory list

--- wp-content/themes/dtw/App/views/blocks/news-item.php
+++ wp-content/themes/dtw/App/views/blocks/news-item.php
@@ -1,13 +1,22 @@
 <?php ?>
-<a class="grid-news-posts__item" href="<?php echo $args['link'];?>" id="<?php echo $args['id'];?>">
+<a class="grid-news-posts__item" href="<?php echo $args['link']; ?>" id="<?php echo $args['id']; ?>">
     <div class="item__img-container">
-        <?php if(!empty($args['src'])): ?>
+        <?php if (!empty($args['src'])): ?>
             <img src="<?php echo $args['src']; ?>" class="img-container__img" alt="<?php echo $args['title']; ?>">
         <?php endif; ?>
     </div>
+    <div class="item__news-cats-row">
+        <div class="news-cats-row__cat-item">
+            <?php foreach ($args['cats'] as $catItem):?>
+            <span>
+                <?php echo $catItem['name'];?>
+            </span>
+            <?php endforeach;?>
+        </div>
+    </div>
     <span class="item__news-title">
-            <?php echo $args['title']; ?>
-        </span>
+        <?php echo $args['title']; ?>
+    </span>
     <p>
         <?php echo $args['desc']; ?>
     </p>
